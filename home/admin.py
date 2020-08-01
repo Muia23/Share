e386b171a179ca4016093c9271267416b8113439
Muia23 <wilfred.muema@gmail.com>
2020-08-01 16:38:13 +0300

    edit the model field adding horizontal filter

--- home/admin.py
+++ home/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from .models import location,category,Post
 
+class PostAdmin(admin.ModelAdmin):
+    filter_horizontal =('location', 'category',)
 # Register your models here.
+admin.site.register(Post,PostAdmin)
 admin.site.register(location)
 admin.site.register(category)
-admin.site.register(Post)
+
 
 
