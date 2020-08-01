3e66e8dd353601f500e595ada1039de9881e3366
Muia23 <wilfred.muema@gmail.com>
2020-08-01 15:37:29 +0300

    connected to Postgres and defined the models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    image_url = models.ImageField(upload_to = 'posts/')
+    title = models.CharField(max_length= 60)
+    caption =models.TextField()
+
+class location(models.Model):
+    name = models.CharField(max_length= 40)
+
+    def __str__(self):
+        return self.name
+
+class category(models.Model):
+    cat = models.CharField(max_length= 30)
+
+    def __str__(self):
+        return self.cat
--- sharer/settings.py
+++ sharer/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'home.apps.HomeConfig',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,10 @@ WSGI_APPLICATION = 'sharer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sharer',
+        'USER': 'willy',
+    'PASSWORD': 'willy',
     }
 }
 
@@ -106,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
