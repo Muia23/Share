0a111939d4ff68d70182360e632971d13c3cb828
Muia23 <wilfred.muema@gmail.com>
2020-08-01 16:15:23 +0300

    set up the admin page and added the models to the page

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import location,category,Post
 
 # Register your models here.
+admin.site.register(location)
+admin.site.register(category)
+admin.site.register(Post)
+
+
