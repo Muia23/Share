b6a55073c32d44feec34c52e6549434014c27da3
Muia23 <wilfred.muema@gmail.com>
2020-08-02 15:27:38 +0300

    create a search results view functiom to render the results

--- home/views.py
+++ home/views.py
@@ -6,3 +6,17 @@ from .models import Post
 def home(request):
     posts = Post.get_posts()
     return render(request,'home.html', {"posts": posts})
+
+def search_results(request):
+
+    if 'post' in request.GET and request.GET["post"]:
+        search_term = request.GET.get("post")
+        searched_posts = Post.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message, "posts": searched_posts})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message": message})
+    
